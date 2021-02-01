9e1f60f70fb89065f0f187457d2162e3825143ff
hanif <muhammad.ihsan.hanif-2018@fst.unair.ac.id>
2021-02-01 19:00:49 +0700

    update convert
    try new method using matplotlib

--- convertFrDicom/convertAll1.py
+++ convertFrDicom/convertAll1.py
@@ -7,8 +7,8 @@ import png
 import PIL # optional
 
 # choose image format
-print('1. JPG \n2. PNG \n3. TIFF ')
-choose = eval(input('Pilihan Format = '))
+# print('1. JPG \n2. PNG \n3. TIFF ')
+# choose = eval(input('Pilihan Format = '))
 
 # Specify the .dcm folder path
 folder_path = "stage_1_test_images"
@@ -17,40 +17,10 @@ folder_path = "stage_1_test_images"
 jpg_path = "JPG_test"
 png_path = "PNG_test"
 tiff_path = "TIFF_test"
-images_path = os.listdir(folder_path)
+images_path = "CT_small.dcm"
 
-for n, image in enumerate(images_path):
-    ds = dicom.dcmread(os.path.join(folder_path, image))
-    arr = ds.pixel_array
-    if choose == 1:
-        image = image.replace('.dcm', '.jpg')
-        # apply modality
-        arr = util.apply_modality_lut(arr, ds)
-        arr = util.apply_voi_lut(arr, ds, index=0)
 
-        cv2.imwrite(os.path.join(jpg_path, image), arr)
-
-    elif choose == 2:
-        image = image.replace('.dcm', '.png')
-        shape = ds.pixel_array.shape
-
-        # Convert to float to avoid overflow or underflow losses.
-        image_2d = ds.pixel_array.astype(float)
-
-        # Rescaling grey scale between 0-255
-        image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
-
-        # Convert to uint
-        image_2d_scaled = np.uint8(image_2d_scaled)
-
-        # Write the PNG file
-        with open(os.path.join(png_path,image), 'wb') as png_file:
-            w = png.Writer(shape[1], shape[0], greyscale=True)
-            w.write(png_file, image_2d_scaled)
-        #cv2.imwrite(os.path.join(png_folder_path, image), pixel_array_numpy)
-    elif choose == 3:
-        image = image.replace('.dcm', '.tiff')
-        
-        cv2.imwrite(os.path.join(tiff_path, image), arr)
-    
-print('{} image converted'.format(n+1))
+ds = dicom.dcmread(images_path)
+arr = ds.pixel_array
+plt.imshow(ds.pixel_array, cmap=plt.cm.bone) 
+plt.show()
